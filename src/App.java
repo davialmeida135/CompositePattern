133164b5a9e173af4e160f2625c91d7689390229
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-04-06 09:58:36 -0300

    nomes

--- src/App.java
+++ src/App.java
@@ -2,19 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         EstanteComposite estante1 = new EstanteComposite("e1");
-        CaixaComposite c1 = new CaixaComposite("c1", "P&G");
+        CaixaComposite caixa1 = new CaixaComposite("c1", "P&G");
         Objeto geladeira1 = new Geladeira("Electrolux",700,2000);
         Objeto geladeira2 = new Geladeira("Consul",1300,3000);
-        Objeto p1 = new ProtetorSolar("Head and shoulder", 60, 15, 40);
-        Objeto p2 = new ProtetorSolar("Pantene", 80, 25, 70);
+        Objeto protetor1 = new ProtetorSolar("Head and shoulder", 60, 15, 40);
+        Objeto protetor2 = new ProtetorSolar("Pantene", 80, 25, 70);
 
 
 
         estante1.add(geladeira1);
         estante1.add(geladeira2);
-        c1.add(p1);
-        c1.add(p2);
-        estante1.add(c1);
+        caixa1.add(protetor1);
+        caixa1.add(protetor2);
+        estante1.add(caixa1);
         
         estante1.imprimir();
     }
